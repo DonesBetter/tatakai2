b97a7a086097ad76449145f3672b9997c0a4c49a
v-wangshiwei <wangshiwei001@chinasofti.com>
2021-09-17 11:17:22 +0800

    DonesBetter

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,6 +7,7 @@ module.exports = {
   theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
   title: "blog",
+  base: ".",
   description: 'web前端技术博客, 菜鸟试水',
   // base: '/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
